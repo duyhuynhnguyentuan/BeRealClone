20ec1842f102d32e835c5ad5ba2e7cac38c7acc3
Huỳnh Nguyễn Tuấn Duy <andyhntd2003@gmail.com>
2023-10-01 10:27:48 +0700

    Initial Commit

---
+++ BeRealClone/BeRealCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeRealCloneApp.swift
+//  BeRealClone
+//
+//  Created by Duy Huỳnh Nguyễn Tuấn on 01/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct BeRealCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
